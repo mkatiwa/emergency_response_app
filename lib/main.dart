26b34f4aabe95cb4ed72d4fef482354b431f7799
jmukamani <j.mukamani@alustudent.com>
2024-05-31 18:31:48 +0200

    text button

--- lib/main.dart
+++ lib/main.dart
@@ -24,23 +24,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to the Emergency Response App'),
+            TextButton(
+              onPressed: () => Navigator.pushNamed(context, '/emergency'),
+              child: Text('Emergency Services'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/profile'),
+              child: Text('Profile'),
+            ),
+            OutlinedButton(
+              onPressed: () => Navigator.pushNamed(context, '/training'),
+              child: Text('First Aid Training'),
+            ),
+          ],
+        ),
+      ),
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
